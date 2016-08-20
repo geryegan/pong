3823b4d37c2650322f5a8c6de02f38fa64479f99
Gery Egan <gery.egan@gmail.com>
2016-08-19 23:59:39 -0700

    canvas coord demo

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var canvas;
 var canvasContext;
 
 window.onload = function(){
+	
 	console.log("Asuh, World")
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d')
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(200,200,50,25);
+	canvasContext.fillStyle = 'green';
+	canvasContext.fillRect(400,300,40,40);
+
 }
