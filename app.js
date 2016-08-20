c105e63214314c3d60a1e4aecfcebf01492e1629
Gery Egan <gery.egan@gmail.com>
2016-08-19 23:51:25 -0700

    canvas set up

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Asuh, World")
+var canvas;
+var canvasContext;
+
+window.onload = function(){
+	console.log("Asuh, World")
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d')
+	canvasContext.fillStyle = 'black';
+	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+}
