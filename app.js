31daae9c634ffa62fa0def6507bd5b9e380e5e47
Gery Egan <gery.egan@gmail.com>
2016-08-20 00:12:29 -0700

    motion demo

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
 
 window.onload = function(){
-	
 	console.log("Asuh, World")
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d')
+	setInterval(drawEverything, 50);
+
+}
+
+function drawEverything(){
+	ballX = ballX + 1;
+
+	console.log(ballX);
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(225,210,200,200);
 	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(200,200,50,25);
-	canvasContext.fillStyle = 'green';
-	canvasContext.fillRect(400,300,40,40);
-
+	canvasContext.fillRect(ballX,200,50,25);
 }
